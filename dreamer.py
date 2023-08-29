3361f7a92120c7ff4109026290161fdd12a29b67
len04 <lbngoc1804@gmail.com>
2023-08-29 16:30:55 -0400

    dreamer fix

--- dreamer.py
+++ dreamer.py
@@ -3,11 +3,11 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Dream Analysis")
+st.set_page_config(page_title="🦙💬 Dreamer")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Dream Analysis Chatbot')
+    st.title('🦙💬 Dreamer')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
@@ -16,30 +16,22 @@ with st.sidebar:
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
-            st.success('Proceed to entering your dream description!', icon='👉')
-    
-    # Model and parameters selection
-    st.subheader('Dream Analysis Model and Parameters')
-    selected_model = st.sidebar.selectbox('Choose a Dream Analysis Model', ['Model 1', 'Model 2', 'Model 3'], key='selected_model')
-    temperature = st.sidebar.slider('Temperature', min_value=0.01, max_value=5.0, value=0.5, step=0.01)
-    # Add other parameter sliders if needed
-
-# Set the Replicate API token
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
-# Store generated dream analysis
-if "dream_analysis" not in st.session_state.keys():
-    st.session_state.dream_analysis = []
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# Clear dream analysis history button
-def clear_analysis_history():
-    st.session_state.dream_analysis = []
-st.sidebar.button('Clear Dream Analysis History', on_click=clear_analysis_history)
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
@@ -53,7 +45,7 @@ def generate_llama2_response(prompt_input):
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
-    return "Dream Analysis: " + output
+    return output
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):

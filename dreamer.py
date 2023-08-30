fe9c7ab3d7f914c03aa43f4341a86001bba0103e
len04 <lbngoc1804@gmail.com>
2023-08-30 13:19:16 -0400

    output

--- dreamer.py
+++ dreamer.py
@@ -31,7 +31,7 @@ for message in st.session_state.messages:
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Tell me about your dream!"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+st.sidebar.button('Clear Dream History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response
 # Refactored from https://github.com/a16z-infra/llama2-chatbot
@@ -45,7 +45,7 @@ def generate_llama2_response(prompt_input):
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
-    return output
+    return "Dream Analysis: " + output
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
